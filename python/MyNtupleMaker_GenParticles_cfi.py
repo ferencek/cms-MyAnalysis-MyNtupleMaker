48447e0bfe736456f9b9d3060b3acb5bff89296a
ferencek <>
2011-11-04 18:20:17 +0000

    reduced the MaxSize from 30 to 20

--- python/MyNtupleMaker_GenParticles_cfi.py
+++ python/MyNtupleMaker_GenParticles_cfi.py
@@ -4,6 +4,6 @@ GenParticles = cms.EDProducer("MyNtupleMaker_GenParticles",
     InputTag = cms.InputTag('genParticles'),
     Prefix = cms.string(''),
     Suffix = cms.string(''),
-    MaxSize = cms.int32(30), # turned off if negative
+    MaxSize = cms.int32(20), # turned off if negative
     PdgIDsOfInterest = cms.vint32(13) # will be stored regardless of the MaxSize parameter
 )
